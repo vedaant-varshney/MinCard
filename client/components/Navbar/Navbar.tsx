e3c9daaeb1504e69c513a8a9e6ca1b91a0ee6926
Vedaant <vedaant.varshney@gmail.com>
2022-06-04 17:29:27 -0400

    navbar progress

--- client/components/Navbar/Navbar.tsx
+++ client/components/Navbar/Navbar.tsx
@@ -3,19 +3,29 @@ import classnames from 'classnames';
 
 import styles from './Navbar.module.scss'
 import MinCardLogo from '@assets/images/MinCardLogo.svg'
+import MagnifyingGlass from '@assets/images/MagnifyingGlass.svg'
 
 export type Props = {
     className?: string;
 }
 
-function Navbar({className}: Props) {
+function Navbar({ className }: Props) {
     return (
         <div className={classnames(styles.Navbar, className)}>
             <div className={styles.navElements}>
-                <MinCardLogo/>
                 {/* <Image src="/../../public/MinCardLogo.svg" alt="" width="40px" height="40px"/> */}
-                <div className={styles.search}>Search for a different card</div>
+                <div className={styles.leftMenu}>
+                    <MinCardLogo className={styles.logo} />
+                    <p>Card Manager</p>
+                </div>
 
+                <div className={styles.search}>
+                    <p>Search for a different card</p>
+                    <MagnifyingGlass/>
+                </div>
+                <div className={styles.rightMenu}>
+                    Profile
+                </div>
             </div>
 
         </div>
